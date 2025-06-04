fa23bb9b7ceb6aece70430e8ddb80467842c8a31
rodrigohiury <rodrigohiury@alu.ufc.br>
2025-06-04 00:01:09 -0300

    Alteração do client e Server

---
+++ client/client.py
@@ -0,0 +1,154 @@
+import json
+import base64
+import hashlib
+import os
+import socket
+import threading
+from dotenv import load_dotenv
+
+load_dotenv()
+HOST_SERVIDOR = os.getenv("HOST_SERVIDOR")
+PORTA_SERVIDOR = int(os.getenv("PORTA_SERVIDOR"))
+TEXT_FORMAT = os.getenv("TEXT_FORMAT")
+TAMANHO_BUFFER = int(os.getenv("TAMANHO_BUFFER"))
+PASTA_SHARED = os.getenv("PASTA_SHARED")
+
+client_id = input("Enter your username: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def treat_package(package):
+    package = json.loads(package)
+    id_chunk = package["id_chunk"]
+    file_chunk = package["file_chunk"]
+    chunk_hash = package["chunk_hash"]
+    
+    # Decode the file chunk from base64
+    file_chunk = base64.b64decode(file_chunk)
+    
+    return id_chunk, file_chunk, chunk_hash
+
+def packageIsOk(chunck, hash):
+    calculated_hash = hashlib.sha256(chunck).hexdigest()
+    
+    if calculated_hash != hash:
+        return False
+    return True
+
+def send_file(archive_name, receiver):
+    if not os.path.exists(archive_name):
+        print(f"[ERROR] File '{archive_name}' does not exist.")
+        return
+
+    with open(archive_name, "rb") as file:
+        file_content = file.read()
+    
+    # Split the file into chunks
+    chunk_size = TAMANHO_BUFFER
+    chunks = [file_content[i:i + chunk_size] for i in range(0, len(file_content), chunk_size)]
+    
+    msg = f"[SEND] {client_id} {archive_name} TO {receiver} {len(chunks)}\n"
+    msg.encode(TEXT_FORMAT)
+    msg_lenght = len(msg)
+    send_lenght = str(msg_lenght).encode(TEXT_FORMAT)
+    send_lenght += b' ' * (64 - len(send_lenght))
+    client.send(send_lenght)
+    
+    for i, chunk in enumerate(chunks):
+
+        chunk_hash = hashlib.sha256(chunk).hexdigest()
+        encoded_chunk = base64.b64encode(chunk).decode(TEXT_FORMAT)
+        
+        package = {
+            "id_chunk": i,
+            "file_chunk": encoded_chunk,
+            "chunk_hash": chunk_hash,
+            "receiver": receiver
+        }
+        
+        package_json = json.dumps(package)
+        client.send(package_json.encode(TEXT_FORMAT))
+        print(f"[INFO] Sending chunk {i} to {receiver}.")
+    
+    client.send("END\n".encode(TEXT_FORMAT))
+    print(f"[INFO] Finished sending file '{archive_name}' to {receiver}.")
+
+def receive_file(total_chunks, filename):
+    last_chunk = -1
+    received_chunks = {}
+    while True:
+        package = client.recv(TAMANHO_BUFFER).decode(TEXT_FORMAT)
+        if package == "END\n":
+            print("[INFO] File transfer completed.")
+            break
+        
+        package_data = json.loads(package)
+        id_chunk = int(package_data["id_chunk"])
+        file_chunk = base64.b64decode(package_data["file_chunk"])
+        chunk_hash = package_data["chunk_hash"]
+        
+        if not packageIsOk(file_chunk, chunk_hash):
+            print(f"[ERROR] Chunk {id_chunk} failed hash verification.")
+            client.send(f"NACK {last_chunk}\n".encode(TEXT_FORMAT))
+            continue
+        
+        client.send(f"ACK {id_chunk}".encode(TEXT_FORMAT))
+
+        if id_chunk == last_chunk+1:
+            received_chunks[id_chunk] = file_chunk
+        
+        print(f"[INFO] Received chunk {id_chunk} successfully.")
+
+    if len(received_chunks) == total_chunks:
+    print("Todos os chunks recebidos. Pronto para remontar.")
+
+    with open(f"{PASTA_SHARED}/{filename}", "wb") as f:
+            for i in range(total_chunks):
+                f.write(received_chunks[i])
+
+def start():
+    while True:
+        cmd = input("Enter command: ")
+        if cmd == "!DISCONNECT":
+            print("[INFO] Ending connection.")
+            client.send("END\n".encode(TEXT_FORMAT))
+            break
+        if cmd.startswith("SEND"):
+            cmd_parts = cmd.split(" ")
+            archive_name = cmd_parts[1]
+            receiver = cmd_parts[3]
+            send_file(archive_name, receiver)
+        elif cmd.startswith("[RECEIVE]"):
+            cmd_parts = cmd.split(" ")
+            total_chunks = int(cmd_parts[3])
+            filename = cmd_parts[2]
+            receive_file(total_chunks, filename)
+        else:
+            print("[ERROR] Unknown command. Use 'SEND <filename> TO <receiver>' to send a file or '!DISCONNECT' to exit.")
+
+def startConnection():
+    try:
+        client.connect((HOST_SERVIDOR, PORTA_SERVIDOR))
+        client.send(client_id.encode(TEXT_FORMAT))
+        thread = threading.Thread(target=start)
+        thread.start()
+        print(f"[CONNECTED] Connected to server at {HOST_SERVIDOR}:{PORTA_SERVIDOR}")
+        print("To end the connection, type 'END\n'")
+        while True:
+            msg = client.recv(64).decode(TEXT_FORMAT)
+            if msg.startswith("[INFO]") or msg.startswith("[ERROR]"):
+                print(msg)
+            elif msg.startswith("END\n"):
+                client.close()
+                break
+    except Exception as e:
+        print(f"[ERROR] Could not connect to server: {e}")
+        return False
+    return True
+
+package = {
+    "id_chunk": id_chunk
+    "file_chunk": file_chunk,
+    chunk_hash: chunk_hash,
+    }
+
--- server/server.py
+++ server/server.py
@@ -1,9 +1,140 @@
-from dotenv import load_dotenv
+import threading
 import os
 import socket
+import json
+import base64
+import hashlib
+from dotenv import load_dotenv
 
 load_dotenv()
+HOST_SERVIDOR = os.getenv("HOST_SERVIDOR")
+PORTA_SERVIDOR = int(os.getenv("PORTA_SERVIDOR"))
+TEXT_FORMAT = os.getenv("TEXT_FORMAT")
+TAMANHO_BUFFER = int(os.getenv("TAMANHO_BUFFER"))
+clients = {}
 
-def run():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((os.getenv("HOST_SERVIDOR"), os.getenv("PORTA_SERVIDOR")))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST_SERVIDOR, PORTA_SERVIDOR))
+server.listen(5)
+
+print(f"[LISTENING] Listening on : {HOST_SERVIDOR}:{PORTA_SERVIDOR}")
+
+def broadcast(message):
+    for client in clients:
+        try:
+            client.send(message.encode('utf-8'))
+        except:
+            # Se der erro (cliente desconectado), remove da lista
+            client.remove(client)
+
+def treat_message(msg):
+    msg = msg.strip(" ")
+    sender = msg[0]
+    file = msg[1]
+    receiver = msg[2]
+    total_chunks = int(msg[3])
+    return sender, file, receiver, total_chunks
+
+def treat_package(package):
+    package = json.loads(package)
+    id_chunk = package["id_chunk"]
+    file_chunk = package["file_chunk"]
+    chunk_hash = package["chunk_hash"]
+    
+    # Decode the file chunk from base64
+    file_chunk = base64.b64decode(file_chunk)
+    
+    return id_chunk, file_chunk, chunk_hash
+
+def packageIsOk(chunck, hash):
+    calculated_hash = hashlib.sha256(chunck).hexdigest()
+    
+    if calculated_hash != hash:
+        return False
+    return True
+
+def receive_file(sender):
+    last_chunk = "-1"
+    client = clients[sender][0]
+    print(f"[INFO] Waiting for file chunks from {sender}...")
+    while True:
+        package = client.recv(TAMANHO_BUFFER).decode(TEXT_FORMAT)
+        if package == "END\n":
+            print("[INFO] File transfer completed.")
+            break
+        
+        package_data = json.loads(package)
+        id_chunk = package_data["id_chunk"]
+        file_chunk = base64.b64decode(package_data["file_chunk"])
+        chunk_hash = package_data["chunk_hash"]
+        
+        if not packageIsOk(file_chunk, chunk_hash):
+            print(f"[ERROR] Chunk {id_chunk} failed hash verification.")
+            client.send(f"NACK {last_chunk}\n".encode(TEXT_FORMAT))
+            continue
+        
+        client.send(f"ACK {id_chunk}".encode(TEXT_FORMAT))
+
+        with open(f"received_chunk_{id_chunk}.bin", "wb") as f:
+            f.write(file_chunk)
+        
+        print(f"[INFO] Received chunk {id_chunk} successfully.")
+
+def handle_client(client_socket, client_address):
+    username = client_socket.recv(64).decode(TEXT_FORMAT)
+    username = username.strip(" ")[0]
+    clients[username] = [client_socket, client_address]
+    msg_connected = f"[INFO] {username} has joined the server."
+    print(msg_connected)
+    broadcast(msg_connected)
+
+    connected = True
+
+    while connected:
+        msg_size = client_socket.recv(64).decode(TEXT_FORMAT)
+        msg_size = int(msg_size)
+        if msg_size:
+            msg = client_socket.recv(int(msg_size)).decode(TEXT_FORMAT)
+            if msg == "END\n":
+                print(f"[INFO] {username} has left the server.")
+                broadcast(f"[INFO] {username} has left the server.")
+                client_socket.send("END\n".encode(TEXT_FORMAT))
+                del clients[username]
+                client_socket.close()
+                connected = False
+                break
+            sender, file, receiver, total_chuncks = treat_message(msg)
+            print(f"[INFO] {sender} wish to send {file} to {receiver}")
+            if receiver in clients:
+                receiver_socket, receiver_address = clients[receiver]
+                receiver_socket.send(f"[RECEIVE] {sender} {file} {total_chuncks}\n".encode(TEXT_FORMAT))
+                client_socket.send("ACK -1".encode(TEXT_FORMAT))
+                receiving = True
+                while receiving:
+                    package = client_socket.recv(TAMANHO_BUFFER).decode(TEXT_FORMAT)
+                    if package != "END\n":
+                        id_chunk, file_chunk, chunk_hash = treat_package(package)
+                        if packageIsOk(file_chunk, chunk_hash):
+                            receiver_socket.send(package)
+                            msg_chunck_ack = f"CHUNCK {id_chunk} ACK"
+                            client_socket.send(msg_chunck_ack.encode(TEXT_FORMAT))
+                    else:
+                        print(f"[INFO] {sender} finished sending {file} to {receiver}")
+                        receiver_socket.send("END\n".encode(TEXT_FORMAT))
+                        client_socket.send("END\n".encode(TEXT_FORMAT))
+                        receiving = False
+            else:
+                print(f"[ERROR] {receiver} not found. File not sent.")
+
+
+
+def start():
+    while True:
+        client_socket, client_address = server.accept()
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        thread.start()
+        print(f"[NEW CONNECTION ESTABLISHED] Active connections: {threading.activeCount() - 1}")
+
+start()
+
+
